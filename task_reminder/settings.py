5c40aceda254f84f0035f1e2f3c64b179e2b7352
Tim Andersson <timalexanderandersson@hotmail.com>
2024-08-31 07:42:11 +0000

    settings key

--- task_reminder/settings.py
+++ task_reminder/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qc-sb@28wgcl*)%jq@7gi5yhmdz24zb*0h+x(+2eo--b8s&qr9'
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
