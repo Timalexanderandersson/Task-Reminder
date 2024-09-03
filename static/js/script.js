d42656013d9e03bb173797fb3a081ba1d1052715
Tim Andersson <timalexanderandersson@hotmail.com>
2024-09-03 07:08:06 +0000

    css style query size

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+
+// Javascript dropdown bar bootstrap
+const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
+
