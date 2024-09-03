c9c23d018501ed882f41080a9c517db5589695b2
Tim Andersson <timalexanderandersson@hotmail.com>
2024-09-03 08:13:10 +0000

    delete in views unnecessary code, added home in base.html,delete unnecessary code in css,and added link for header title h1

--- task_app/views.py
+++ task_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.views import generic
 from .models import UserTask, Taskmodel, Taskcomment
 
 
@@ -7,7 +6,6 @@ def Userforviews(request):
     user = Taskmodel.objects.all()
     user_task = UserTask.objects.all()
     task_comment = Taskcomment.objects.all()
-    template_name = "task_app/index.html"
 
     return render(request, 'index.html', {
         'user': user,
