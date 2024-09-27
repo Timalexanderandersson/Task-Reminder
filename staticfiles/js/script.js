8df12ee437b2a17d94becd9000f86076c58a53f1
Tim Andersson <timalexanderandersson@hotmail.com>
2024-09-27 12:34:29 +0000

    added for collectstatic in settings, created runtime,txt

---
+++ staticfiles/js/script.js
@@ -0,0 +1,44 @@
+//Getting the Add button.
+const addbutton = document.querySelector('.addbutton');
+// Function for addbutton in the front page for adding task.
+addbutton.addEventListener('click', function(){
+    let taskform = document.querySelector('.formdetails');
+    if(taskform.style.display == 'none' || taskform.style.display == ''){
+        taskform.style.display = 'block';
+        taskform.style.paddingLeft = '5vh';
+        addbutton.innerText = "X";
+        addbutton.style.backgroundColor ='orange';
+        addbutton.style.padding = '5px';
+    } else{
+        taskform.style.display = 'none';
+        addbutton.innerText = "Add";
+        addbutton.style.backgroundColor ='rgb(42, 156, 42)';
+    }
+})
+
+
+//Getting the Add buttons to task list.
+const addbuttontask = document.querySelectorAll('.task-buttons')
+// Function for all the add buttons in the front page.
+for (let i = 0; i < addbuttontask.length; i++){
+    addbuttontask[i].addEventListener('click', function(){
+        let taskinside = document.querySelectorAll('.task-details')[i];
+      
+            if(taskinside.style.display == 'none' || taskinside.style.display == ''){
+                taskinside.style.display = 'block'
+                taskinside.style.paddingLeft = '5vh'
+                addbuttontask[i].innerText = "X";
+                addbuttontask[i].style.backgroundColor ='orange';
+                addbuttontask[i].style.padding = '5px';
+        
+            } else{
+                taskinside.style.display = 'none'
+                addbuttontask[i].innerText = "Add";
+                addbuttontask[i].style.backgroundColor ='rgb(42, 156, 42)';
+            }  
+    })
+}
+
+// Javascript dropdown bar bootstrap
+const dropdownElementList = document.querySelectorAll('.dropdown-toggle');
+const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl));
--- task_reminder/settings.py
+++ task_reminder/settings.py
@@ -53,6 +53,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,6 +138,7 @@ USE_TZ = True
 
 STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static'),]
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
