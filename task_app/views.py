b4356d92f52960e6e47178ae7b198a3f584bb78b
Tim Andersson <timalexanderandersson@hotmail.com>
2024-09-27 07:20:29 +0000

    added new code in task_edit for showing task, and fixed index.html css,and views

--- task_app/views.py
+++ task_app/views.py
@@ -6,22 +6,21 @@ from django.contrib import messages
 #Takes user/visitor to the front page.
 def homepage(request):
 
-    return render(request, 'index.html',)
+    return render(request, 'task_edit.html',)
 
 
 # Views for adding to the task list.
 def taskpush(request):
+    form = PostUser(request.POST)
     allinfo = TaskUser.objects.filter()
     if request.method == 'POST':
         form = PostUser(request.POST)
-        form_user = PostUser(request.POST)
         if form.is_valid():
             form.save()
             return redirect('Homepage')
     else:
-        form = PostForm()
         form_user = PostUser()
-        return render(request, 'index.html', {
+        return render(request, 'task_edit.html', {
         'form': form,
         'allinfo': allinfo,
         })
