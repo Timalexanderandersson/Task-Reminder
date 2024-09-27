fa1025c7232ea4db6038f9b123935a40cff51e4b
Tim Andersson <timalexanderandersson@hotmail.com>
2024-09-27 09:05:38 +0000

    added to delete_site, edit_task for delete and edit. and views for delete and edit. css for styling the task bar. urls added.

--- task_app/urls.py
+++ task_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
  
 urlpatterns = [
     path('', views.homepage, name='Homepage'),
-    path('tasks', views.taskpush, name='task-create'),
-    
+    path('tasks/', views.taskpush, name='task-create'),
+    path('delete/<int:pk>/', views.DeleteTask.as_view(), name='delete_site'),
+    path('update/<int:pk>/', views.UpdateTask.as_view(), name='updating_task'),
 ]
--- task_app/views.py
+++ task_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
+from django.views.generic.edit import DeleteView, UpdateView
 from .models import TaskUser
 from .forms import PostUser
+from django.urls import reverse_lazy
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
 #Takes user/visitor to the front page.
 def homepage(request):
@@ -9,21 +13,39 @@ def homepage(request):
     return render(request, 'task_edit.html',)
 
 
-# Views for adding to the task list.
+# Adding to the task list as user.
 def taskpush(request):
-    form = PostUser(request.POST)
     allinfo = TaskUser.objects.filter()
     if request.method == 'POST':
-        form = PostUser(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('Homepage')
+            form = PostUser(request.POST)
+            if form.is_valid():
+               tasks = form.save(commit=False)
+               tasks.user = request.user
+               tasks.save()
+               messages.success(request, f'You added a task.')
+               return redirect('task_create')
+            else:
+                messages.error(request, 'Did not fill in all.')
     else:
-        form_user = PostUser()
-        return render(request, 'task_edit.html', {
+            form = PostUser()
+    return render(request, 'task_edit.html', {
         'form': form,
-        'allinfo': allinfo,
+        'allinfo':allinfo,
         })
 
+# Delete tasks.
+class DeleteTask(LoginRequiredMixin,SuccessMessageMixin,DeleteView):
+    model = TaskUser
+    context_object_name = 'tasks'
+    template_name = 'delete_site.html'
+    success_url = reverse_lazy('task-create')
+    success_message = 'Deleted the task'
 
-
+#Updating tasks
+class UpdateTask(LoginRequiredMixin,SuccessMessageMixin,UpdateView):
+    model = TaskUser
+    form_class = PostUser
+    template_name = 'edit_task.html'
+    context_object_name = 'tasks'
+    success_message = 'Updated task'
+    success_url = reverse_lazy('task-create')
