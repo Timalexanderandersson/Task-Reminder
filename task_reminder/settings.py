d9d7e822eb7f05a3cc68a49e95b5e586c43afd0b
Tim Andersson <timalexanderandersson@hotmail.com>
2024-10-01 14:25:28 +0000

    clean code in html base, and index

--- task_reminder/settings.py
+++ task_reminder/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -92,6 +93,9 @@ WSGI_APPLICATION = 'task_reminder.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
